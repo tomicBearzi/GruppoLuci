9dc14c5c0684914bf32d48c798abf2395fd52d02
danielw <162579099+danielwgs@users.noreply.github.com>
2025-04-07 21:01:27 +0200

    Add files via upload

--- src/login.php
+++ src/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="icon" href="logo.png" type="image/x-icon" />
+    <link rel="icon" href="logo.svg" type="image/x-icon" />
     <link rel="stylesheet" href="./css/login/style.css">
     <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700,900&f[]=rajdhani@300,400,500,600,700&display=swap" rel="stylesheet">
 </head>
