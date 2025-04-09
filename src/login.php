e0ab80b6e702bfa03f4392419435cf01306e205d
danielw <daniele.dinucci@allievi.bearzi.it>
2025-04-09 22:36:06 +0200

    sistemazione index

--- src/login.php
+++ src/login.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="icon" href="logo.svg" type="image/x-icon" />
+    <link rel="icon" href="img/logo.svg" type="image/x-icon" />
     <link rel="stylesheet" href="./css/login/style.css">
     <link href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,700,900&f[]=rajdhani@300,400,500,600,700&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="top-bar">
-        <img class="logo" src="logo.svg">
+        <img class="logo" src="img/logo.svg">
     </div>
     <div class="container">
 
